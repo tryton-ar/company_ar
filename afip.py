bd0c560631829da3b721b3739bb573f0b4e4f3ea
Luciano Rossi <lukio@gcoop.coop>
2017-08-25 18:06:04 -0300

    typo party_ar to company_ar. Add user_afip to this module

--- afip.py
+++ afip.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# This file is part of the party_ar module for Tryton.
+# This file is part of the company_ar module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 
